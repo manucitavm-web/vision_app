03218e45f5c9b97c34d3fac3376e32064e9f237c
manucitavm-web <manucitavm@gmail.com>
2026-04-09 13:51:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def encode_image(image_file):
 
 st.set_page_config(page_title="Analisis de imagen", layout="centered", initial_sidebar_state="collapsed")
 # Streamlit page setup
-st.title("Análisis de Imagen:🤖🏞️")
+st.title("Análisis de Imagenes:🤖🏞️")
 ke = st.text_input('Ingresa tu Clave')
 os.environ['OPENAI_API_KEY'] = ke
 
